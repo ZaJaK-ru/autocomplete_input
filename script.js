bfc545f0d9fa9afc3106f9273d500f1ecbb1df82
zvasib <zvasib@ya.ru>
2022-10-10 21:44:49 +0300

    refactor

--- script.js
+++ script.js
@@ -1,59 +1,59 @@
 const input = document.querySelector('.autocomplete-form__input');
+const autoComplete = document.querySelector('.autocomplete-form__list');
 const selectedRepo = document.querySelector('.selected-repo');
 
 const debounce = (fn, debounceTime) => {
   let timeout;
 
   return function() {
-    const fnCall = () => { fn.apply(this, arguments); }
     clearTimeout(timeout);
-    timeout = setTimeout(fnCall, debounceTime);
+    timeout = setTimeout(()=>fn(), debounceTime);
   }
 };
 
-function removeAutoComplete() {
-  let items = document.querySelectorAll(".autocomplete-form__list-items");
-  items.forEach(item => item.remove());
-}
-
 async function getRepositories(query) {
-  return await fetch(`https://api.github.com/search/repositories?q=${query}&per_page=5`)
-    .then(responce => responce.json())
-    .then(data => data.items);
+  let response = await fetch(`https://api.github.com/search/repositories?q=${query}&per_page=5`);
+   
+  if (response.status == 200) {
+    let data = await response.json();
+    return data.items;   
+  }
+
+  throw new Error(response.status);
 }
 
 function addRepoCard(repo) {
   selectedRepo.insertAdjacentHTML('beforeend', 
     `<div class="selected-repo__card">
-      <ul class="selected-repo__card-list">
-        <li class="selected-repo__card-list-item">Name: ${repo.name}</li>
-        <li class="selected-repo__card-list-item">Owner: ${repo.owner.login}</li>
-        <li class="selected-repo__card-list-item">Stars: ${repo.stargazers_count}</li>
-      </ul>
-    <button class="selected-repo__card-delete"></button>
+      <pre class="selected-repo__card-list">
+        Name: ${repo.name}
+        Owner: ${repo.owner.login}
+        Stars: ${repo.stargazers_count}
+      </pre>
+      <button class="selected-repo__card-delete"></button>
     </div>`
   );
-  removeAutoComplete();
+  autoComplete.replaceChildren();
   input.value = '';
 }
 
 function handleInput(e) {
-  removeAutoComplete();
+  autoComplete.replaceChildren();
 
-  if (input.value != '') {
-    let repositories = [];
+  if (input.value === '') return;
 
-    getRepositories(input.value)
-    .then(data => {
-      for (let item of data) {
+  getRepositories(input.value)
+    .then(data => { 
+      data.forEach(item => {
         let listItem = document.createElement("li");
-        listItem.classList.add("autocomplete-form__list-items");
-        listItem.addEventListener('click', () => addRepoCard(item));
-        listItem.innerText = item.name;
-        document.querySelector(".autocomplete-form__list").appendChild(listItem);
-      }  
+        let listItemBtn = document.createElement("button");
+        listItemBtn.classList.add("autocomplete-form__list-item-btn");
+        listItemBtn.addEventListener('click', () => addRepoCard(item));
+        listItemBtn.innerText = item.name;
+        listItem.appendChild(listItemBtn);
+        autoComplete.appendChild(listItem);       
+      });
     });
-  }
 }
 
 input.addEventListener('keyup', debounce(handleInput, 500));
